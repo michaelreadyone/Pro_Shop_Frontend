26af9e56d3394f855b3d5ba5ad3ad448a66da643
mike <michael56653556@gmail.com>
2021-10-22 21:09:13 -0400

    Section5:Adding To Shopping Cart

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
         <Container>
           <Route path="/" component={HomeScreen} exact/>
           <Route path="/products/:id" component={ProductScreen} />
+          {/* question mark makes this parameter optional */}
+          <Route path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
       <Footer />
--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,22 +1,39 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Button,
+  Card,
+  Form,
+} from "react-bootstrap";
 import Rating from "../components/Rating";
 import { useDispatch, useSelector } from "react-redux";
 import { listProductDetails } from "../actions/productActions";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 
-function ProductScreen({ match }) {
-  console.log(match.params.id);
+function ProductScreen({ match, history }) {
+  // console.log(match.params.id);
+
+  const [qty, setQty] = useState(1);
+
   const dispatch = useDispatch();
   const productDetails = useSelector((state) => state.productDetails);
   const { error, loading, product } = productDetails;
+  // console.log(product)
 
   useEffect(() => {
     dispatch(listProductDetails(match.params.id));
   }, [dispatch, match]);
+
+  const addToCartHandler = () => {
+    // console.log("add to cart: ", match.params.id);
+    history.push(`/cart/${match.params.id}?qty=${qty}`)
+  };
 
   return (
     <div>
@@ -74,8 +91,33 @@ function ProductScreen({ match }) {
                   </Row>
                 </ListGroup.Item>
 
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col xs="auto" className="my-1">
+                        <Form.Control
+                          as="select"
+                          value={qty}
+                          onChange={(e) => setQty(e.target.value)}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            // ...Array() what does this mean?
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+
                 <ListGroup.Item>
                   <Button
+                    // what's the difference to add () or not add ()
+                    // why adding () the function will be automatically activated when the component is renderred
+                    onClick={addToCartHandler}
                     className="btn-block"
                     disabled={product.countInStock === 0}
                     type="button"
